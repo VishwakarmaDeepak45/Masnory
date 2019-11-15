8a475e1e7e2bb76fe2afe73e66d975bdd9a4c34e
Kiran Deore <kiran.deore@tacto.in>
2019-11-15 19:15:10 +0530

    Enhance: Remove unused Dependency and added few usefull property.

--- App.js
+++ App.js
@@ -9,13 +9,13 @@
 import React, { Component } from 'react';
 import {View, Text} from 'react-native'
 import MasonryList from 'react-native-masonry-list';
-import MesonryLayout from 'react-native-masonry-layout';
 
 export default class App extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
+      //we can pass URI inside the data and also provide the dimensions or height width directoly on that particular data.
       data: [
         {
           uri:
@@ -34,7 +34,55 @@ export default class App extends Component {
         {
           uri:
             "https://hips.hearstapps.com/amv-prod-cad-assets.s3.amazonaws.com/vdat/submodels/dodge_challenger_dodge-challenger_2019-1545059179866.jpg"
-        },]   
+        },
+        {
+          uri:
+            "https://www.bmw-speedmotorwagen.in/sites/default/files/styles/nostyle/public/slider_banner_image/2018-02/M4Coupe-Header_Banner_17.jpg?itok=zmJWURhi"
+        },
+       {
+          url:
+            "https://img.etimg.com/thumb/msid-67103187,width-1200,height-900,resizemode-4,imgsize-96644/car-getty.jpg"
+        },
+        {
+          uri:
+            "https://hips.hearstapps.com/amv-prod-cad-assets.s3.amazonaws.com/vdat/submodels/dodge_challenger_dodge-challenger_2019-1545059179866.jpg"
+        },
+        {
+          uri:
+            "https://www.bmw-speedmotorwagen.in/sites/default/files/styles/nostyle/public/slider_banner_image/2018-02/M4Coupe-Header_Banner_17.jpg?itok=zmJWURhi"
+        },
+       {
+          url:
+            "https://img.etimg.com/thumb/msid-67103187,width-1200,height-900,resizemode-4,imgsize-96644/car-getty.jpg"
+        },
+        {
+          uri:
+            "https://hips.hearstapps.com/amv-prod-cad-assets.s3.amazonaws.com/vdat/submodels/dodge_challenger_dodge-challenger_2019-1545059179866.jpg"
+        },
+        {
+          uri:
+            "https://www.bmw-speedmotorwagen.in/sites/default/files/styles/nostyle/public/slider_banner_image/2018-02/M4Coupe-Header_Banner_17.jpg?itok=zmJWURhi"
+        },
+       {
+          url:
+            "https://img.etimg.com/thumb/msid-67103187,width-1200,height-900,resizemode-4,imgsize-96644/car-getty.jpg"
+        },
+        {
+          uri:
+            "https://hips.hearstapps.com/amv-prod-cad-assets.s3.amazonaws.com/vdat/submodels/dodge_challenger_dodge-challenger_2019-1545059179866.jpg"
+        },
+        {
+          uri:
+            "https://www.bmw-speedmotorwagen.in/sites/default/files/styles/nostyle/public/slider_banner_image/2018-02/M4Coupe-Header_Banner_17.jpg?itok=zmJWURhi"
+        },
+       {
+          url:
+            "https://img.etimg.com/thumb/msid-67103187,width-1200,height-900,resizemode-4,imgsize-96644/car-getty.jpg"
+        },
+        {
+          uri:
+            "https://hips.hearstapps.com/amv-prod-cad-assets.s3.amazonaws.com/vdat/submodels/dodge_challenger_dodge-challenger_2019-1545059179866.jpg"
+        },  ]   
        };
   }
 
@@ -44,38 +92,21 @@ export default class App extends Component {
     const {data} = this.state;
     return (
       <View style={{flex:1}}>
-      {/* <MesonryLayout 
-          ref="data"
-          header={
-            <View>
-                <Text>Header text</Text>
-            </View>
-          }
-          footer ={
-            <View>
-                <Text>Footer text</Text>
-            </View>
-          }
-          
-          // columns={3} 
-          renderItem={(item)=>
-            <View>
-                
-            </View>
-          }
-      /> */}
-           <MasonryList
-                  renderIndividualFooter={
+          <MasonryList
+                //By default it takes 2 column.
+                columns={3} 
+                //By default the spacing is 1. increase it as per your requirements.
+                spacing={1}
+                //like footer we can alo use header for that. but it take the vlue inside the view.
+                renderIndividualFooter={
                   <View>
                     <Text>Footer text</Text>
-                </View>
-                  }
-                  
-
-                  images={data}
-              />
+                  </View>
+                }
+                //images props takes array only not array data.
+                images={data}
+            />
       </View>
-      
     );
   }
 }
